a34cfd495a6336bc7bf69d3d2812e8c167ef9f23
Leeeyounghwan <lee.yhwan226@gmail.com>
2023-12-04 15:20:09 +0900

    feat: 백준 1436번 풀이 추가

---
+++ 2023.12/BOJ_1436.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+cnt = 0
+num = 1
+
+while True:
+    if "666" in str(num):
+        cnt = cnt + 1
+
+    if cnt == N:
+        print(num)
+        break
+
+    num = num + 1
