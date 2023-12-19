e8ff50bf20c711b4730f8b7e9867b070565cd030
Lee Younghwan <lee.yhwan226@gmail.com>
2023-12-19 22:47:21 +0900

    feat: 백준 10872번 풀이 추가

---
+++ 2023.12/12.19/BOJ_10872.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+def factorial(N):
+    if N > 1:
+        return N * factorial(N - 1)
+    else:
+        return 1
+    
+print(factorial(N))
