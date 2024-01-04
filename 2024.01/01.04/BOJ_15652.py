9d68c1da1b6b0db498a91e1c7e85329051b2fc09
Lee Younghwan <lee.yhwan226@gmail.com>
2024-01-04 21:53:09 +0900

    feat: 백준 15652번 풀이 추가

---
+++ 2024.01/01.04/BOJ_15652.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M  = map(int, input().split())
+
+li = []
+
+def dfs(start):
+    if len(li) == M:
+        print(" ".join(map(str, li)))
+        return
+    
+    for i in range(start, N+1):
+        li.append(i)
+        dfs(i)
+        li.pop()
+
+dfs(1)
